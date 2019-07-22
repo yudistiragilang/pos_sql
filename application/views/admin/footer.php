22448a874eb2395cacfbad2ee9051bc7fbf4b41e
Yudhistira Gilang <yudhistiragilang22@gmail.com>
2019-07-22 11:50:06 +0700

    commit pertama

---
+++ application/views/admin/footer.php
@@ -0,0 +1,8 @@
+
+	<footer>
+        <div class="row">
+            <div class="col-lg-12">
+                <p style="text-align:center;">Copyright &copy; <?php echo date('Y');?> by SQL</p>
+            </div>
+        </div>
+    </footer>
