42feef55f6f8cca0894c8a75fb94bc18790dbd93
yudistiragilang <yudhistiragilang22@gmail.com>
2019-07-24 23:37:04 +0700

    set time jakarta

--- application/config/config.php
+++ application/config/config.php
@@ -1,5 +1,7 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
+date_default_timezone_set("Asia/Jakarta");
+
 /*
 |--------------------------------------------------------------------------
 | Base Site URL
