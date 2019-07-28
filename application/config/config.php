dd1fc697f62312b93df4072f15576c6114538705
yudistiragilang <yudhistiragilang22@gmail.com>
2019-07-28 18:18:06 +0700

    session user 5 menit

--- application/config/config.php
+++ application/config/config.php
@@ -255,7 +255,7 @@ $config['encryption_key'] = 'mfikrisetiadi';
 |
 */
 $config['sess_cookie_name']		= 'ci_session';
-$config['sess_expiration']		= 7200;
+$config['sess_expiration']		= ‭300‬;
 $config['sess_expire_on_close']	= FALSE;
 $config['sess_encrypt_cookie']	= FALSE;
 $config['sess_use_database']	= FALSE;
