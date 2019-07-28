835a7f7e58f2b9ae21eee08fd577011803a29d4e
yudistiragilang <yudhistiragilang22@gmail.com>
2019-07-28 16:11:49 +0700

    import from excel Unit of measure

--- application/controllers/admin/Uom.php
+++ application/controllers/admin/Uom.php
@@ -79,4 +79,37 @@ class Uom extends CI_Controller{
 
 	}
 
+	function import_uom(){
+
+		include_once ( APPPATH."libraries/Excel_reader.php");
+	    $data = new Spreadsheet_Excel_Reader($_FILES['userfile']['tmp_name']);
+	     
+	    $jumlah_sukses = 0;
+	    $jumlah_gagal = 0;
+
+	    for ($i=3; $i <= ($data->rowcount($sheet_index=0)); $i++){ 
+
+	    	$uom = $data->val($i, 2);
+	    	$des_uom = $data->val($i, 3);
+
+	    	$simpan = $this->m_uom->simpan_uom($uom, $des_uom);
+
+	    	// audit 
+			$this->m_global->audit_master('Insert', 'tbl_uom', $simpan);
+			// audit
+
+	    	if ($simpan) {
+	    		$jumlah_sukses = $jumlah_sukses + 1;
+	    	} else {
+	    		$jumlah_gagal = $jumlah_gagal + 1;
+	    	}
+	      
+	    }
+
+	    echo $this->session->set_flashdata('sukses','<label class="label label-success">Unit of Measure Sukses = '.$jumlah_sukses.'</label>');
+	    echo $this->session->set_flashdata('gagal','<label class="label label-danger">Unit of Measure Gagal = '.$jumlah_gagal.'</label>');
+	    redirect('admin/uom');
+
+	}
+
 }
